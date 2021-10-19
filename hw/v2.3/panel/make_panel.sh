2c8f947433369cafc3f4b97d09d49c503f43f7f8
Greg Davill <greg.davill@gmail.com>
2021-10-20 08:29:57 +1030

    Update PCBs to remove small vias
    - Regenerate 1x panel
    - Add gerbers

--- hw/v2.3/panel/make_panel.sh
+++ hw/v2.3/panel/make_panel.sh
@@ -1,17 +1,17 @@
 PYTHONPATH=/usr/lib/kicad-nightly/lib/python3/dist-packages/ \
 kikit panelize \
-    --layout 'hspace: 5mm; cols: 2' \
+    --layout 'hspace: 5mm; cols: 1' \
     --tabs 'spacing: 15mm' \
-    --cuts 'type: mousebites; drill: 0.35mm; spacing: 0.5mm; offset: -0.2mm; prolong: 2 mm' \
+    --cuts 'type: mousebites; drill: 0.4mm; spacing: 0.8mm; offset: -0.2mm; prolong: 2 mm' \
     --framing 'type: tightframe; width: 3mm' \
     --post 'millradius: 0.8mm' \
     ../main-board/gps-logger-main.kicad_pcb gps-logger-main-panel.kicad_pcb
 
 PYTHONPATH=/usr/lib/kicad-nightly/lib/python3/dist-packages/ \
 kikit panelize \
-    --layout 'hspace: 5mm; vspace: 2mm; rows: 2; cols: 2' \
+    --layout 'hspace: 5mm; vspace: 5mm; rows: 1; cols: 1' \
     --tabs 'spacing: 20mm' \
-    --cuts 'type: mousebites; drill: 0.35mm; spacing: 0.5mm; offset: -0.2mm; prolong: 2 mm' \
+    --cuts 'type: mousebites; drill: 0.4mm; spacing: 0.8mm; offset: -0.2mm; prolong: 2 mm' \
     --framing 'type: tightframe; width: 5mm' \
     --post 'millradius: 0.8mm' \
     --debug 'trace: True' \
